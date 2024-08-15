4dd38532d734500c706dbb3b99d80ed9001cc146
van Thiết <tretrauthiet@gmail.com>
2024-08-15 15:07:39 +0700

    create project

---
+++ Client/src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div>
+         <h1 className="text-red-500">hello</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ Server/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+
+
+// Middleware
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.send("Welcome To Api");
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
