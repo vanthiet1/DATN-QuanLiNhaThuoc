493dae7c400c8efeae6b8e6a4f8627525a6ee459
congquoc7 <tuyetquoc207@gmail.com>
2024-08-22 19:46:11 +0700

    Delete Server/src/routers/blog.js

--- Server/src/routers/blog.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const blogController = require('../controllers/blogController/blog');
-
-router.post("/:id", blogController.addBlog);
-router.put('/edit/:id', blogController.updateBlog);
-router.get("/comments", blogController.getAllBlogs);
-router.delete("/:id", blogController.deleteBlog);
-
-module.exports = router;
