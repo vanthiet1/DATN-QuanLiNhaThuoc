6c58561792ed1aa9d11bb3dd3655f6658e1741ea
congquoc7 <tuyetquoc207@gmail.com>
2024-08-22 19:45:01 +0700

    Delete Server/src/routers/banner.js

--- Server/src/routers/banner.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const bannerController = require('../controllers/bannerController/banner');
-
-router.post("/:id", bannerController.addBanner);
-router.get("/comments", bannerController.getAllBanner);
-router.delete("/:id", bannerController.deleteBanner);
-
-module.exports = router;
