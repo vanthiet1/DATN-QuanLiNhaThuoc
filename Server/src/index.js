2a5fe3cccbcef26193d70ed339e749b1a1bdbacf
van Thiết <tretrauthiet@gmail.com>
2024-08-24 09:44:25 +0700

    create router blog and comment

--- Server/src/index.js
+++ Server/src/index.js
@@ -31,6 +31,10 @@ const ProductRouter = require('./routers/product');
 const CategoryRouter = require('./routers/category');
 const SubCategoryRouter = require('./routers/subCategory');
 const BrandRouter = require('./routers/brand');
+const BlogRouter = require('./routers/blog');
+const CommentRouter = require('./routers/comments');
+
+
 
 app.use('/api/v1/order', OrderRouter);
 app.use('/api/v1/order-detail', OrderDetailRouter);
@@ -46,6 +50,10 @@ app.use('/api/v1/category', CategoryRouter);
 app.use('/api/v1/subCategory', SubCategoryRouter);
 app.use('/api/v1/product', ProductRouter);
 app.use('/api/v1/brand', BrandRouter);
+app.use('/api/v1/blog', BlogRouter);
+app.use('/api/v1/comment', CommentRouter);
+
+
 
 // Start server
 const PORT = process.env.PORT || 5000;
