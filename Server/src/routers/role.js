9e292549075d13530a4be176e77899c734a30cb7
van Thiết <tretrauthiet@gmail.com>
2024-09-13 16:08:21 +0700

    refactor role

--- Server/src/routers/role.js
+++ Server/src/routers/role.js
@@ -1,12 +1,10 @@
 const Router = require('express').Router()
 const RoleController = require('../controllers/roleController/role');
 
-    
-// Role: lấy tất cả / tạo sp / lấy chi tiết / cập nhật / xóa
 Router.get('/', RoleController.getRoles);
-Router.post('/create', RoleController.createRole);
-Router.get('/detail/:id', RoleController.getRoleById);
-Router.put('/edit/:id', RoleController.updateRole);
-Router.delete('/delete/:id', RoleController.deleteRole);
+Router.post('/', RoleController.createRole);
+Router.get('/:id', RoleController.getRoleById);
+Router.put('/:id', RoleController.updateRole);
+Router.delete('/:id', RoleController.deleteRole);
 
 module.exports = Router;
