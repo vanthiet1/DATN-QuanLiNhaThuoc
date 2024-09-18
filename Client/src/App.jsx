e4080672e135b510ce61064d8c813b8a7229eafc
van Thiết <tretrauthiet@gmail.com>
2024-09-19 00:42:58 +0700

    up

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,28 +1,46 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-
-const UserIcon = () => {
-  return (
-    <svg fill='currentColor' viewBox='0 0 20 20' class='w-5 h-5'>
-      <path d='M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z'></path>
-    </svg>
-  );
-};
-
+import { Input } from './components/ui/input';
+import Index from './modules/auth';
 const App = () => {
-  const handleOnclick = (e) => {
-    alert('xin chào!');
-  };
+  const data = [
+    {
+      id: 1,
+      first_name: "Janessa",
+      last_name: "Kneesha",
+      email: "jkneesha0@about.com",
+      gender: "Female",
+      ip_address: "152.114.187.73"
+    },
+    {
+      id: 2,
+      first_name: "Hakim",
+      last_name: "Reinhard",
+      email: "hreinhard1@mapquest.com",
+      gender: "Genderfluid",
+      ip_address: "62.144.244.28"
+    },
+  ];
   return (
     <div>
       <ToastContainer />
-      {/* auth test */}
-      {/* <Index/> */}
-      <Button addClassNames={'text-gray-200 py-10 px-[60px]'} onClick={(e) => handleOnclick()} rightIcon={UserIcon}>
-        sss
-      </Button>
+      <Input
+        type="text"
+        placeholder="Input 1"
+        leftIcon={<svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+        </svg>}
+        addClassNames={'bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-[#333] dark:focus:ring-blue-500 dark:focus:border-blue-500 pl-[30px] focus:ring-[1px] duration-200 relative'}
+      />
+      <br />
+      <Input
+        type="text"
+        placeholder="Input 2"
+        addClassNames={'bg-gray-50  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500  focus:ring-[1px] duration-200 relative'}
+      />
+<Index></Index>
     </div>
+
   );
 };
 
