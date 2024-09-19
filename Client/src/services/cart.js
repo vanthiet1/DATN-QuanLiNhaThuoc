8becf4a309a906343032bcac767da402ba026e12
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-09-19 11:26:48 +0700

    tổ chức lại mã code ở phần utils và services

--- Client/src/services/cart.js
+++ Client/src/services/cart.js
@@ -1,41 +1,41 @@
-import http from "../utils/http";
-import { showToastError,showToastSuccess } from "../configs/toastConfig";
-const cartServices = {
-     addToCart: async (requestBody)=>{
-        try {
-            const {data} =  await http.get('/cart/',requestBody)
-            console.log(data);
-            return data
-        } catch (error) {
-            console.log(error.message);     
-        }
-     },
-     getCartByUserId: async ()=>{
-        try {
-             const {data} =  await http.get(`/cart/66e01288685b2acb7f3d1a79`)
-             console.log(data);
-             return data
-        } catch (error) {
-            console.log(error.message);     
-        }
-     },
-      deleteCartByUserId: async (requestParams)=>{
-        try {
-            const {data} =  await http.delete(`/cart/${requestParams}`)
-            console.log(data);
-            return data
-        } catch (error) {
-         console.log(error.message);
-        }
-     },
-     deleteProductCartByUserId: async (requestParams)=>{
-        try {
-            const {data} =  await http.delete(`/cart/product/${requestParams}`)
-            console.log(data);
-            return data
-        } catch (error) {
-            console.log(error.message);     
-        }
-     }
-}
-export default cartServices
+// import http from "../utils/http";
+// import { showToastError,showToastSuccess } from "../configs/toastConfig";
+// const cartServices = {
+//      addToCart: async (requestBody)=>{
+//         try {
+//             const {data} =  await http.get('/cart/',requestBody)
+//             console.log(data);
+//             return data
+//         } catch (error) {
+//             console.log(error.message);     
+//         }
+//      },
+//      getCartByUserId: async ()=>{
+//         try {
+//              const {data} =  await http.get(`/cart/66e01288685b2acb7f3d1a79`)
+//              console.log(data);
+//              return data
+//         } catch (error) {
+//             console.log(error.message);     
+//         }
+//      },
+//       deleteCartByUserId: async (requestParams)=>{
+//         try {
+//             const {data} =  await http.delete(`/cart/${requestParams}`)
+//             console.log(data);
+//             return data
+//         } catch (error) {
+//          console.log(error.message);
+//         }
+//      },
+//      deleteProductCartByUserId: async (requestParams)=>{
+//         try {
+//             const {data} =  await http.delete(`/cart/product/${requestParams}`)
+//             console.log(data);
+//             return data
+//         } catch (error) {
+//             console.log(error.message);     
+//         }
+//      }
+// }
+// export default cartServices
