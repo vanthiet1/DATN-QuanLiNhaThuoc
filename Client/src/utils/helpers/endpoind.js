afddd0aa5067f17ba15e8c4c2b4735d6aed66166
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-09-23 16:55:31 +0700

    cập nhật endpoind cho phần định tuyến api

--- Client/src/utils/helpers/endpoind.js
+++ Client/src/utils/helpers/endpoind.js
@@ -1,5 +1,21 @@
 const END_POIND_API = {
-   AUTH:'/auth',
-   CART:'/cart'
-}
-export default END_POIND_API
+  AUTH: '/auth',
+  CART: '/cart',
+  PRODUCT: '/product',
+  ORDER: '/order',
+  ORDER_DETAILS: '/order-details',
+  ADDRESS: '/address',
+  COUPON: '/coupon',
+  BLOG: '/blog',
+  CATEGORY: '/category',
+  SUB_CATEGORY: '/subCategory',
+  BRAND: '/brand',
+  BANNER: '/banner',
+  COMMENT: '/comment',
+  EMAIL: '/email',
+  ROLE: '/role',
+  PHARMACY: '/pharmacy',
+  PAYMENT_METHOD: '/payment-method'
+};
+
+export default END_POIND_API;
