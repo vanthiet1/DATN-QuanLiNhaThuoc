2f9a85dcaab56f8723a05833de5b2d0319c5971b
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-09-23 12:51:37 +0700

    tạo component SelectBox

--- Client/src/components/ui/form/index.js
+++ Client/src/components/ui/form/index.js
@@ -1,3 +1,4 @@
 export { default as InputRadio } from './InputRadio';
 export { default as InputText } from './InputText';
 export { default as Textarea } from './Textarea.jsx';
+export { default as SelectBox } from './SelectBox.jsx';
