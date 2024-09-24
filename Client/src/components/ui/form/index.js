7d796b7eb9bd176f264553260e8dbca5be8bc1d0
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-09-24 12:39:27 +0700

    tạo component FileInput

--- Client/src/components/ui/form/index.js
+++ Client/src/components/ui/form/index.js
@@ -3,4 +3,4 @@ export { default as InputText } from './InputText';
 export { default as Textarea } from './Textarea.jsx';
 export { default as SelectBox } from './SelectBox.jsx';
 export { default as ErrorMessage } from './ErrorMessage';
-
+export { default as FileInput } from './FileInput';
