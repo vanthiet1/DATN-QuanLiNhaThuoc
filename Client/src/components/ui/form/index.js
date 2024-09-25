e396f51868e6597f80e85a7b8783880ff7e7fb12
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-09-25 21:14:46 +0700

    tạo component InputDate

--- Client/src/components/ui/form/index.js
+++ Client/src/components/ui/form/index.js
@@ -4,3 +4,4 @@ export { default as Textarea } from './Textarea.jsx';
 export { default as SelectBox } from './SelectBox.jsx';
 export { default as ErrorMessage } from './ErrorMessage';
 export { default as FileInput } from './FileInput';
+export { default as InputDate } from './InputDate';
