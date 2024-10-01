a4ea982d15c9dd4e952de66b9d59dfb93c89f641
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-10-01 13:31:03 +0700

    bổ sung tuyến đường pharmacy

--- Server/src/index.js
+++ Server/src/index.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const http = require('http');
 const cors = require('cors');
-const {initIo} = require('./socket/socketManager')
+const { initIo } = require('./socket/socketManager');
 require('dotenv').config();
 const app = express();
 const connectDB = require('./db/connectDB');
@@ -24,7 +24,6 @@ app.use(
 
 connectDB();
 initIo(server);
-
 
 app.get('/', (req, res) => {
   res.send('Welcome To Api');
@@ -50,9 +49,7 @@ const CommentRouter = require('./routers/comments');
 const VerifyRouter = require('./routers/vertifyEmail');
 const StaffRouter = require('./routers/staff');
 const MessageRouter = require('./routers/message');
-
-
-
+const PharmacyRouter = require('./routers/pharmacy');
 
 app.use('/api/v1/order', OrderRouter);
 app.use('/api/v1/order-details', OrderDetailRouter);
@@ -74,10 +71,7 @@ app.use('/api/v1/comment', CommentRouter);
 app.use('/api/v1/email', VerifyRouter);
 app.use('/api/v1/staff', StaffRouter);
 app.use('/api/v1/message', MessageRouter);
-
-
-
-
+app.use('/api/v1/pharmacy', PharmacyRouter);
 
 // Start server
 const PORT = process.env.PORT || 5000;
