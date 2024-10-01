eaf77bf4595f1f66fe00d75bcb03aa7c5a33154b
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-10-01 14:39:26 +0700

    cập nhật component card product admin

--- Client/src/components/card/CardProductAdmin.jsx
+++ Client/src/components/card/CardProductAdmin.jsx
@@ -7,6 +7,9 @@ import productServices from '../../services/productService';
 import { useConfirmDialog } from '../dialog/ConfirmDialogContext';
 import { ProductAdminContext } from '../../admin/product/AllProduct';
 
+const imageSrcDefault =
+  'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyODggMTgwIiB3aWR0aD0iMjg4IiBoZWlnaHQ9IjE4MCI+CiAgPHJlY3Qgd2lkdGg9IjI4OCIgaGVpZ2h0PSIxODAiIGZpbGw9IiNEQURBREFGRiI+PC9yZWN0PgogIDx0ZXh0IHg9IjUwJSIgeT0iNTAlIiBkb21pbmFudC1iYXNlbGluZT0ibWlkZGxlIiB0ZXh0LWFuY2hvcj0ibWlkZGxlIiBmb250LWZhbWlseT0ibW9ub3NwYWNlIiBmb250LXNpemU9IjI2cHgiIGZpbGw9IiM0MDQwNDRGRiI+Tm8gSW1hZ2U8L3RleHQ+ICAgCjwvc3ZnPg==';
+
 const CardProductAdmin = ({ product, addClassname, ...props }) => {
   const confirmDialog = useConfirmDialog();
 
@@ -30,24 +33,30 @@ const CardProductAdmin = ({ product, addClassname, ...props }) => {
 
   return (
     <div className='card-product border border-solid border-gray-200 rounded overflow-hidden'>
-      <Image src={images[0]?.url_img} alt={name} lazyLoad addClassNames={'h-[180px] w-full object-fill'}></Image>
-      <div className='card-product-hover:bg-slate-500 p-2'>
-        <h3 className='text-lg font-semibold leading-tight text-gray-900 truncate mt-2 my-3'>{name}</h3>
+      <Image
+        src={images[0]?.url_img}
+        alt={name}
+        fallbackSrc={imageSrcDefault}
+        lazyLoad
+        addClassNames={'h-[180px] w-full object-fill'}
+      ></Image>
+      <div className='card-product-hover:bg-gray-500 p-2'>
+        <h3 className='font-semibold leading-tight text-gray-700 line-clamp-2 mt-2 my-3'>{name}</h3>
         <div className='flex'>
           {price_old > 0 && (
-            <p className='text-gray-400 my-1 line-through text-base mr-4'>{formatsHelper.currency(price_old)}</p>
+            <p className='text-gray-400 my-1 line-through text-sm mr-4'>{formatsHelper.currency(price_old)}</p>
           )}
-          <p className='text-indigo-400 font-semibold my-1 text-base'>{formatsHelper.currency(price_distcount)}</p>
+          <p className='text-indigo-500 font-semibold my-1 text-sm'>{formatsHelper.currency(price_distcount)}</p>
         </div>
         <div className='flex items-center justify-between py-2 transition-colors'>
-          <Button addClassNames='text-slate-600 w-9 hover:text-blue-500'>
+          <Button addClassNames='text-gray-600 w-9 hover:text-blue-500'>
             {<AppIcons.EyeIcon width='20' height='20' />}
           </Button>
           <div className='flex items-center gap-2'>
-            <Button addClassNames={'text-slate-600 hover:text-green-500'}>
+            <Button addClassNames={'text-gray-600 hover:text-teal-500'}>
               {<AppIcons.EditIcon width='20' height='20' />}
             </Button>
-            <Button addClassNames={'text-slate-600 hover:text-pink-500'} onClick={() => handleDeleteProduct()}>
+            <Button addClassNames={'text-gray-600 hover:text-rose-500'} onClick={() => handleDeleteProduct()}>
               {<AppIcons.TrashBinIcon width='20' height='20' />}
             </Button>
           </div>
