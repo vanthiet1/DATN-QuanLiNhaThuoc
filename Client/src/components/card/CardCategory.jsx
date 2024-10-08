6ef4beb2f1253704533f2eaf7294498c0635d8a8
van Thiết <tretrauthiet@gmail.com>
2024-10-08 20:40:00 +0700

    tạo card cate và product

---
+++ Client/src/components/card/CardCategory.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+const CardCategory = ({ name, description, image, categoriesId }) => {
+    return (
+        <Link to={`/danh-muc/${categoriesId}`}>
+            <div className="flex bg-red-100 rounded-[5px] justify-between p-2">
+                <div>
+                    <span className="block font-bold text-[17px]">{name}</span>
+                    <span className="block">{description}</span>
+                </div>
+                <div>
+                    <img className="w-[100px]" src={image} alt={image || "danh mục"} />
+                </div>
+            </div>
+        </Link>
+    );
+};
+
+export default CardCategory;
