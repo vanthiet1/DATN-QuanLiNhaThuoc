55dae0fd92d7cef8f56705c3b88e63dbc69e21ec
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-10-08 20:52:44 +0700

    cập nhật lại model cho address , coupon , order ...

--- Server/src/models/ordersModels/order.js
+++ Server/src/models/ordersModels/order.js
@@ -4,10 +4,11 @@ const orderSchema = new mongoose.Schema(
   {
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     order_date: { type: Date, required: true },
-    status: { type: Number, required: true, default: 1 },
+    status: { type: Number, required: true, default: 1, enum: [1, 2, 3, 4] },
     total_price: { type: Number, required: true },
     total_quantity: { type: Number, required: true },
-    coupon_id: { type: mongoose.Schema.Types.ObjectId, required: true },
+    sale_type: { type: String, required: true, enum: ['online', 'off'] },
+    coupon_id: { type: mongoose.Schema.Types.ObjectId },
     payment_method_id: { type: mongoose.Schema.Types.ObjectId, required: true },
     shipping_address_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Address', required: true }
   },
