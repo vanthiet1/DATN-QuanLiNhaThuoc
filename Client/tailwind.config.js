bdd93bdbb551f09ac962688738ce64bf10f5f708
van Thiết <tretrauthiet@gmail.com>
2024-10-17 11:27:07 +0700

    create animation

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideDown: {
+          '0%': { transform: 'translateY(-100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        slideDown: 'slideDown 0.5s ease-out',
+      },
+    },
   },
   plugins: [],
 }
