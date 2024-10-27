5014364a7f53a89e4d9294991979387e0d3953b1
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-10-27 18:13:53 +0700

    cập nhật icon mới

--- Client/src/components/ui/icon/index.jsx
+++ Client/src/components/ui/icon/index.jsx
@@ -549,7 +549,21 @@ const LocationIcon = ({ width = '24', height = '24', addClassNames = '' }) => {
 };
 const ArrowDown = ({ width = '24', height = '24', addClassNames = '' }) => {
   return (
-    <svg className={addClassNames} width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M7.293 9.293a1 1 0 0 1 1.414 0L12 12.586l3.293-3.293a1 1 0 1 1 1.414 1.414l-4 4a1 1 0 0 1-1.414 0l-4-4a1 1 0 0 1 0-1.414Z" fill="#000000"></path></svg>
+    <svg
+      className={addClassNames}
+      width={width}
+      height={height}
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M7.293 9.293a1 1 0 0 1 1.414 0L12 12.586l3.293-3.293a1 1 0 1 1 1.414 1.414l-4 4a1 1 0 0 1-1.414 0l-4-4a1 1 0 0 1 0-1.414Z'
+        fill='#000000'
+      ></path>
+    </svg>
   );
 };
 
@@ -575,23 +589,21 @@ const ArrowLeftCircle = ({ width = '24', height = '24', addClassNames = '' }) =>
 const ArrowRight = ({ width = '24', height = '24', addClassNames = '' }) => {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor" 
-      strokeWidth="2"
-      strokeLinecap="round" 
-      strokeLinejoin="round" 
-      className={`icon-arrow ${addClassNames}`} 
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={`icon-arrow ${addClassNames}`}
       width={width}
       height={height}
     >
-      <polyline points="9 18 15 12 9 6"></polyline>
+      <polyline points='9 18 15 12 9 6'></polyline>
     </svg>
   );
 };
-
-
 
 const PrinterIcon = ({ width = '24', height = '24', addClassNames = '' }) => {
   return (
@@ -607,6 +619,26 @@ const PrinterIcon = ({ width = '24', height = '24', addClassNames = '' }) => {
       <path
         fillRule='evenodd'
         d='M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 0 0 3 3h.27l-.155 1.705A1.875 1.875 0 0 0 7.232 22.5h9.536a1.875 1.875 0 0 0 1.867-2.045l-.155-1.705h.27a3 3 0 0 0 3-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0 0 18 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25ZM16.5 6.205v-2.83A.375.375 0 0 0 16.125 3h-8.25a.375.375 0 0 0-.375.375v2.83a49.353 49.353 0 0 1 9 0Zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 0 1-.374.409H7.232a.375.375 0 0 1-.374-.409l.526-5.784a.373.373 0 0 1 .333-.337 41.741 41.741 0 0 1 8.566 0Zm.967-3.97a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H18a.75.75 0 0 1-.75-.75V10.5ZM15 9.75a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V10.5a.75.75 0 0 0-.75-.75H15Z'
+        clipRule='evenodd'
+      />
+    </svg>
+  );
+};
+
+const CkeckIcon = ({ width = '24', height = '24', addClassNames = '' }) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 24 24'
+      fill='currentColor'
+      className={addClassNames}
+      aria-hidden='true'
+      width={width}
+      height={height}
+    >
+      <path
+        fillRule='evenodd'
+        d='M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z'
         clipRule='evenodd'
       />
     </svg>
@@ -649,6 +681,7 @@ const icons = {
   ArrowDown,
   ArrowRight,
   PrinterIcon,
+  CkeckIcon
 };
 
 export default icons;
