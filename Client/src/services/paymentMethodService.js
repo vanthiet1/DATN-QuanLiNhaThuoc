fff8fe00193f56cc1927acdda2cfe8d7de12e416
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-10-27 18:12:20 +0700

    cập nhật và tái cấu trúc mã cho một số file service

--- Client/src/services/paymentMethodService.js
+++ Client/src/services/paymentMethodService.js
@@ -5,29 +5,36 @@ import { showToastError, showToastSuccess } from '../configs/toastConfig';
 const paymentMethodServices = {
   addPaymentMethod: async (requestBody) => {
     try {
-      const { data } = await http.post(`/${END_POIND_API.PAYMENT_METHOD}/create`, requestBody);
+      const { data } = await http.post(`${END_POIND_API.PAYMENT_METHOD}/create`, requestBody);
       showToastSuccess(data.message || 'Thêm phương thức thanh toán thành công');
       return data;
     } catch (error) {
-      showToastError(error.response.data.message);
+      console.log(error.message);
+    }
+  },
+  getAllPaymentMethod: async () => {
+    try {
+      const { data } = await http.get(`${END_POIND_API.PAYMENT_METHOD}`);
+      console.log(data);
+      return data;
+    } catch (error) {
       console.log(error.message);
     }
   },
 
   getPaymentMethodById: async (requestParams) => {
     try {
-      const { data } = await http.get(`/${END_POIND_API.PAYMENT_METHOD}/${requestParams}`);
+      const { data } = await http.get(`${END_POIND_API.PAYMENT_METHOD}/${requestParams}`);
       console.log(data);
       return data;
     } catch (error) {
-      showToastError(error.response.data.message);
       console.log(error.message);
     }
   },
 
   deletePaymentMethod: async (requestParams) => {
     try {
-      const { data } = await http.delete(`/${END_POIND_API.PAYMENT_METHOD}/${requestParams}`);
+      const { data } = await http.delete(`${END_POIND_API.PAYMENT_METHOD}/${requestParams}`);
       showToastSuccess(data.message || 'Xóa phương thức thanh toán thành công');
       return data;
     } catch (error) {
@@ -35,10 +42,10 @@ const paymentMethodServices = {
       console.log(error.message);
     }
   },
-  
+
   updatePaymentMethod: async (requestParams) => {
     try {
-      const { data } = await http.delete(`/${END_POIND_API.PAYMENT_METHOD}/${requestParams}`);
+      const { data } = await http.delete(`${END_POIND_API.PAYMENT_METHOD}/${requestParams}`);
       showToastSuccess(data.message || 'Cập nhật phương thức thanh toán thành công');
       return data;
     } catch (error) {
