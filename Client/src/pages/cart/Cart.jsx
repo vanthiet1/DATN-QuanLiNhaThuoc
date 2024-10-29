04441a1c2468a815d68697a40f9dff891c3cbc13
van Thiết <tretrauthiet@gmail.com>
2024-10-29 20:38:28 +0700

    thêm link cho button xem sản phẩm

--- Client/src/pages/cart/Cart.jsx
+++ Client/src/pages/cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState , useRef } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '../../components/ui/button';
 import { CartContext } from '../../contexts/CartContext';
@@ -14,16 +14,16 @@ import { useConfirmDialog } from '../../components/dialog/ConfirmDialogContext';
 import debounce from '../../hooks/useDebounce';
 import { SpinnerLoading } from '../../components/ui/loaders';
 import useSrcollTop from '../../hooks/useSrcollTop';
-
+import { PATH_ROUTERS_CLIENT } from '../../utils/constant/routers';
 const Cart = () => {
   useSrcollTop()
   const { cart, getProductCart } = useContext(CartContext);
   const { user } = useContext(UserContext) || { user: null };
   const confirmDialog = useConfirmDialog();
   const [quantities, setQuantities] = useState({});
-  const [loading, setLoading] = useState(false); 
+  const [loading, setLoading] = useState(false);
   const debounceTimeout = useRef(null);
- 
+
   useEffect(() => {
     const initialQuantities = cart?.reduce((acc, item) => {
       acc[item.productId._id] = item.quantity;
@@ -35,7 +35,7 @@ const Cart = () => {
   const handleQuantityChange = (productId, change) => {
     setQuantities((prev) => {
       const newQuantity = Math.max(1, (prev[productId] || 0) + change);
-      
+
       clearTimeout(debounceTimeout.current);
       debounceTimeout.current = setTimeout(() => {
         handleUpdateQuantity(productId, newQuantity);
@@ -49,14 +49,14 @@ const Cart = () => {
   };
 
   const handleUpdateQuantity = async (productId, quantity) => {
-    setLoading(true); 
+    setLoading(true);
     try {
       await cartServices.updateQuantityCart({
         userId: user._id,
         productId,
         quantity,
       });
-      await getProductCart(user._id); 
+      await getProductCart(user._id);
     } catch (error) {
       console.error("Error updating quantity:", error);
     } finally {
@@ -77,14 +77,14 @@ const Cart = () => {
     }
   };
 
-  
+
   return (
     <>
       {loading && (
         <div className=" flex justify-center items-center bg-opacity-50 z-50 fixed left-0  w-[100%] bg-[#d8d8d8] h-full top-0">
           <div className="loader">
-          <SpinnerLoading/>
-          .</div> 
+            <SpinnerLoading />
+            .</div>
         </div>
       )}
       {cart?.length > 0 ? (
@@ -101,11 +101,11 @@ const Cart = () => {
                   </div>
 
                   <div className="flex-1">
-                   <Link to={`/product/${productCart?.productId?.slug}`}>
-                   <p className="text-[16px] font-semibold text-gray-800">
-                      {productCart.productId.name}
-                    </p>
-                   </Link>
+                    <Link to={`/product/${productCart?.productId?.slug}`}>
+                      <p className="text-[16px] font-semibold text-gray-800">
+                        {productCart.productId.name}
+                      </p>
+                    </Link>
                   </div>
 
                   <div className="w-max flex items-center gap-3">
@@ -148,45 +148,48 @@ const Cart = () => {
               <CheckoutForm />
             </div>
             <div className='lg:col-span-1'>
-        {/* <Transactions /> */}
-        <PaymentMethod />
-        <CouponSection />
-        <div className='border p-4 mt-6'>
-          <div className='font-bold text-lg pb-2'>Thông tin đơn hàng</div>
-          <div className='flex justify-between py-1'>
-            <p className=' text-base font-medium'>Tổng đơn hàng</p>
-            <p className='text-base font-medium'>1,800.000đ</p>
-          </div>
-          <div className='flex justify-between py-1'>
-            <p className=' text-base font-medium'>Phí vận chuyển</p>
-            <p className='text-base font-medium'>35.000đ</p>
-          </div>
-          <div className='flex justify-between py-1'>
-            <p className='text-base font-medium'>Giảm giá</p>
-            <p className='text-base font-medium'>35.000đ</p>
-          </div>
-          <div className=' border-t-2 mt-4 mb-4'>
-            <div className='flex justify-between pt-4'>
-              <p className='text-base font-semibold'>Tổng Tiền</p>
-              <p className='text-base font-medium'>35.000đ</p>
+              {/* <Transactions /> */}
+              <PaymentMethod />
+              <CouponSection />
+              <div className='border p-4 mt-6'>
+                <div className='font-bold text-lg pb-2'>Thông tin đơn hàng</div>
+                <div className='flex justify-between py-1'>
+                  <p className=' text-base font-medium'>Tổng đơn hàng</p>
+                  <p className='text-base font-medium'>1,800.000đ</p>
+                </div>
+                <div className='flex justify-between py-1'>
+                  <p className=' text-base font-medium'>Phí vận chuyển</p>
+                  <p className='text-base font-medium'>35.000đ</p>
+                </div>
+                <div className='flex justify-between py-1'>
+                  <p className='text-base font-medium'>Giảm giá</p>
+                  <p className='text-base font-medium'>35.000đ</p>
+                </div>
+                <div className=' border-t-2 mt-4 mb-4'>
+                  <div className='flex justify-between pt-4'>
+                    <p className='text-base font-semibold'>Tổng Tiền</p>
+                    <p className='text-base font-medium'>35.000đ</p>
+                  </div>
+                  <Button size='l' rounded='s' addClassNames='flex bg-blue-500 text-white mt-4 w-full justify-center item-center hover:bg-blue-600 transition-all duration-300'>Thanh toán</Button>
+                </div>
+              </div>
             </div>
-            <Button size='l' rounded='s' addClassNames='flex bg-blue-500 text-white mt-4 w-full justify-center item-center hover:bg-blue-600 transition-all duration-300'>Thanh toán</Button>
           </div>
         </div>
-      </div>
-    </div>
-          </div>
       ) : (
         <div className="flex justify-center items-center">
-             <div>
-               <div className="flex justify-center">
-               <img className='w-[100px]' src="https://www.medigoapp.com/assets/images/empty-cart.png?v=19-10-15-10-2024" alt="" />
-               </div>
-               <span className='block text-center py-4 '>Chưa có sản phẩm trong giỏ hàng</span>
-              <div className="flex  justify-center">
-              <Button addClassNames='bg-[#2563eb] p-3 text-[16px] text-[#fff] rounded-lg'>Xem sản phẩm</Button>
-              </div>
-             </div>
+          <div>
+            <div className="flex justify-center">
+              <img className='w-[100px]' src="https://www.medigoapp.com/assets/images/empty-cart.png?v=19-10-15-10-2024" alt="" />
+            </div>
+            <span className='block text-center py-4 '>Chưa có sản phẩm trong giỏ hàng</span>
+            <div className="flex  justify-center">
+              <Link to={`/${PATH_ROUTERS_CLIENT.ALL_PRODUCT
+                }`}>
+                <Button addClassNames='bg-[#2563eb] p-3 text-[16px] text-[#fff] rounded-lg'>Xem sản phẩm</Button>
+              </Link>
+            </div>
+          </div>
         </div>
       )}
     </>
