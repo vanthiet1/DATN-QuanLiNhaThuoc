da7170bea8999ae987ccd0dae6cbccdb3b8200b1
van Thiết <tretrauthiet@gmail.com>
2024-11-13 00:09:07 +0700

    refactor code

--- Client/src/components/layouts/Footer.jsx
+++ Client/src/components/layouts/Footer.jsx
@@ -8,9 +8,6 @@ import logoTiktok from '../../assets/images/logo/logoTiktok.png'
 import logoPhay from '../../assets/images/logo/logo CH Phay.png'
 import logoAppStore from '../../assets/images/logo/logo App store.png'
 import logoCongthuong from '../../assets/images/logo/logo Congthuong.png'
-
-
-
 const Footer = () => {
   return (
     <footer className="bg-[#f9f9f9] text-[#27272a] p-10">
@@ -22,9 +19,7 @@ const Footer = () => {
             className="w-[250px] h-auto hover:scale-90 transition-transform duration-300 ease-in-out"
           />
         </div>
-
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 px-10 gap-6 cursor-pointer">
-
           <div>
             <h1 className="font-bold mb-3">Hỗ Trợ Khách Hàng</h1>
             <ul className="space-y-2">
@@ -39,25 +34,23 @@ const Footer = () => {
               <li>Miễn trừ trách nhiệm nội dung</li>
             </ul>
           </div>
-
-          {/* Cột 2 */}
           <div>
             <h1 className="font-bold mb-3">Về Bình An Dược</h1>
             <ul className="space-y-2">
-            <Link to={`${PATH_ROUTERS_CLIENT.CONTACT}`}>
-              <li  className='pb-2'>Giới thiệu</li>
-            </Link>
-            <Link className='' to={`${PATH_ROUTERS_CLIENT.ABOUT}`} >
-              <li>Liên hệ</li>
-            </Link>
+              <Link to={`${PATH_ROUTERS_CLIENT.CONTACT}`}>
+                <li className='pb-2'>Giới thiệu</li>
+              </Link>
+              <Link to={`${PATH_ROUTERS_CLIENT.ABOUT}`} >
+                <li>Liên hệ</li>
+              </Link>
               <li>Phòng xét nghiệm</li>
-              <li>Tính bmi online</li>
+              <Link to={`${PATH_ROUTERS_CLIENT.BMICALCULATOR}`} >
+                <li className='py-1'>Tính bmi online</li>
+              </Link>
               <li>Công cụ tính ngày dự sinh</li>
               <li>Công cụ tính ngày rụng trứng</li>
             </ul>
           </div>
-
-          {/* Cột 3 */}
           <div>
             <h1 className="font-bold mb-3">Hợp Tác và Liên Kết</h1>
             <ul className="space-y-2">
@@ -68,12 +61,9 @@ const Footer = () => {
               <li>Danh sách bác sĩ</li>
             </ul>
           </div>
-
-          {/* Cột 4 */}
           <div>
             <h1 className="font-bold mb-3">Danh Mục Sản Phẩm</h1>
             <ul className="space-y-2">
-      
               <li>Chăm sóc sắc đẹp</li>
               <li>Chăm sóc cá nhân</li>
               <li>Thiết bị y tế</li>
@@ -84,8 +74,6 @@ const Footer = () => {
               <li>Tin tức</li>
             </ul>
           </div>
-
-          {/* Cột 5 */}
           <div>
             <div className="text-center">
               <h1 className="font-bold mb-3">Kết Nối Với Chúng Tôi</h1>
@@ -130,20 +118,15 @@ const Footer = () => {
               <img src={logoAppStore} alt="App Store" className="w-[120px] h-auto cursor-pointer" />
               <img src={logoPhay} alt="Google Play" className="w-[120px] h-auto cursor-pointer" />
             </div>
-
             <h1 className="font-bold mb-3 mt-9">Chứng Nhận Bởi</h1>
             <div className="flex space-x-2">
-              <img src={logoCongthuong} alt="App Store" className="w-[120px] h-auto cursor-pointer" />              
+              <img src={logoCongthuong} alt="App Store" className="w-[120px] h-auto cursor-pointer" />
             </div>
           </div>
         </div>
-
-        {/* Certification */}
         <div className="mt-8 flex justify-center">
           <img src={logo} alt="Certification" className="w-[150px] h-auto" />
         </div>
-
-        {/* Footer  */}
         <div className="mt-8 text-center text-xs text-[#6b7280]">
           <p>© 2014 - 2025 Công Ty Trách Nhiệm Hữu Hạn Bình An Dược</p>
           <p>Địa chỉ: Y1 Hồng Lĩnh, Phường 15, Quận 10, TPDN - Hotline: 1800 2247</p>
