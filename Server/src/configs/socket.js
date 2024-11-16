4a29eb83f22f5939f940d196e872c36c589baabd
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-11-16 14:51:45 +0700

    cấu hình socket

---
+++ Server/src/configs/socket.js
@@ -0,0 +1,49 @@
+const { Server } = require('socket.io');
+
+let io;
+const socket = async (httpServer) => {
+  try {
+    io = new Server(httpServer, {
+      cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST']
+      }
+    });
+
+    io.on('connection', (socket) => {
+      console.log('Người dùng kết nối:', socket.id);
+
+      socket.on('registerRoom', (userRole) => {
+        console.log(userRole);
+        if (userRole === 'admin') {
+          socket.join('admin');
+          console.log('Người dùng là admin');
+        } else if (userRole === 'staff') {
+          socket.join('staff');
+          console.log('Người dùng là staff');
+        } else {
+          socket.join('customer');
+          console.log('Người dùng là customer');
+        }
+      });
+
+      socket.on('disconnect', async () => {
+        console.log('Người dùng ngắn kết nối:', socket.id);
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getIoSocket = () => {
+  if (!io) {
+    throw Error('Có lỗi khi get io');
+  }
+  return io;
+};
+
+module.exports = {
+  socket,
+  getIoSocket
+};
