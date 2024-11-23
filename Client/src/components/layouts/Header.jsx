80d7a8430d0da2ac24f139bb886eccf1e32e54ad
van Thiết <tretrauthiet@gmail.com>
2024-11-23 12:37:20 +0700

    reponsive cho header và nav

--- Client/src/components/layouts/Header.jsx
+++ Client/src/components/layouts/Header.jsx
@@ -28,16 +28,18 @@ const Header = () => {
     const [searchKeyword, setSearchKeyword] = useState('');
     const [products, setProducts] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [isShowCategory, setIsShowCategory] = useState(false);
 
 
-const redirectOrder = ()=>{
-    navigate(`${PATH_ROUTERS_CLIENT.ACCOUNT}`)
-    setTabIndex(3)
-}
-const redirectHistoryOrder= ()=>{
-    navigate(`${PATH_ROUTERS_CLIENT.ACCOUNT}`)
-    setTabIndex(4)
-}
+
+    const redirectOrder = () => {
+        navigate(`${PATH_ROUTERS_CLIENT.ACCOUNT}`)
+        setTabIndex(3)
+    }
+    const redirectHistoryOrder = () => {
+        navigate(`${PATH_ROUTERS_CLIENT.ACCOUNT}`)
+        setTabIndex(4)
+    }
 
     const getProductSearch = async () => {
         if (!searchKeyword.trim()) return setProducts([]);
@@ -79,18 +81,20 @@ const redirectHistoryOrder= ()=>{
         <div className={`ease-in-out sticky top-0 z-30`}>
             <header>
                 <div className='bg-[#2563EB]'>
-                    <div className="flex items-center justify-between p-2 w-[90%] m-auto">
-                        <div className='flex gap-2 items-center'>
+                    <div className="flex items-center justify-between p-2 w-[90%] m-auto max-md:w-full ">
+                        <div className='flex gap-2 items-center  max-md:flex-col'>
                             <Link to={PATH_ROUTERS_CLIENT.HOMEPAGE}>
-                                <img className='w-[200px]' src={Logo} alt="Logo" />
+                                <div className='w-[200px] max-md:w-[130px] max-md:pb-1'>
+                                    <img className='w-[100%]' src={Logo} alt="Logo" />
+                                </div>
                             </Link>
-                            <div className='relative'>
+                            <div className='relative max-md:w-full max-md:absolute max-md:left-0 max-md:top-[55px]'>
                                 <div className="absolute top-1/2 left-5 transform -translate-x-1/2 -translate-y-1/2 " onClick={handleSearchQueryProduct}>
                                     <AppIcons.SearchIcons addClassNames='text-[#a8a8a8] text-[20px] ' />
                                 </div>
                                 <InputText
                                     onChange={handleChange}
-                                    addClassNames={"rounded-[5px] w-[400px] h-[50px] pl-[10px] text-[16px] pl-10"}
+                                    addClassNames={"max-md:w-full rounded-[5px] w-[400px] h-[50px] pl-[10px] text-[16px] pl-10"}
                                     placeholder="Bạn đang tìm gì?"
                                     onKeyDown={handleKeyDown}
                                 />
@@ -102,7 +106,7 @@ const redirectHistoryOrder= ()=>{
                                     )}
                                 </div>
                                 {searchKeyword && (
-                                    <div className="absolute bg-[#fff] w-full rounded-[5px] z-30 p-5">
+                                    <div className="absolute bg-[#fff] w-full rounded-[5px] z-30 p-5 h-[500px] overflow-auto">
                                         <div>
                                             {products?.length > 0 ? (
                                                 products.map((product) => (
@@ -123,13 +127,13 @@ const redirectHistoryOrder= ()=>{
                                     </div>
                                 )}
                             </div>
-                     
+
                         </div>
-                        <div className='flex gap-5 items-center '>
+                        <div className='flex gap-5 items-center max-md:gap-3 max-md:pr-5'>
                             {user ? (
-                                <div className="flex items-center justify-end w-[230px] ">
+                                <div className="flex items-center justify-end w-[230px] max-md:w-[100%]">
                                     <div className="flex items-center relative gap-3 group">
-                                        <span className='text-[#fff]'>Chào bạn</span>
+                                        <span className='text-[#fff] max-md:hidden '>Chào bạn</span>
                                         <div className=' p-1 cursor-pointer'>
                                             {user?.avatar ? (
                                                 <img className='w-[40px] rounded-[50%]' src={user?.avatar} alt="avatar" />
@@ -137,11 +141,11 @@ const redirectHistoryOrder= ()=>{
                                                 <AppIcons.UserIcon addClassNames='text-[#2563EB] bg-[#fff] rounded-[50%] w-[40px] h-[40px] p-1 ' />
                                             )}
                                         </div>
-                                        <div className='hidden group-hover:block absolute top-[25px] pt-[35px] z-10'>
+                                        <div className='hidden group-hover:block absolute top-[25px] pt-[35px] z-10 max-md:left-[-100px]'>
                                             <div className='w-[250px] bg-[#fff] shadow p-3 rounded-[5px] '>
                                                 <span className='block'>Xin chào</span>
                                                 <span className='block font-bold'>{user?.fullname}</span>
-                                                {user?.role_id?.role_Name.includes("admin") && (
+                                                {(user?.role_id?.role_Name.includes("admin") || user?.role_id?.role_Name.includes("staff")) && (
                                                     <Link to={`/${PATH_ROUTERS_ADMIN.DASHBOARD}`}>
                                                         <div className="flex mt-3 items-center gap-2 cursor-pointer pb-1 group hover:text-[#2563EB] duration-200">
                                                             <span><AppIcons.UserIcon addClassNames='text-gray-800' /></span>
@@ -155,12 +159,10 @@ const redirectHistoryOrder= ()=>{
                                                         <span>Thông tin tài khoản</span>
                                                     </div>
                                                 </Link>
-                                             
-                                                    <div className="flex mt-3 items-center gap-2 cursor-pointer pb-1 group hover:text-[#2563EB] duration-200" onClick={redirectOrder}>
-                                                        <span><AppIcons.OderIcon addClassNames='text-gray-800' /></span>
-                                                        <span>Đơn hàng xử lí</span>
-                                                    </div>
-                                                
+                                                <div className="flex mt-3 items-center gap-2 cursor-pointer pb-1 group hover:text-[#2563EB] duration-200" onClick={redirectOrder}>
+                                                    <span><AppIcons.OderIcon addClassNames='text-gray-800' /></span>
+                                                    <span>Đơn hàng xử lí</span>
+                                                </div>
                                                 <div className="flex mt-3 items-center gap-2 cursor-pointer pb-1 group hover:text-[#2563EB] duration-200" onClick={redirectHistoryOrder}>
                                                     <span><AppIcons.BanknotesIcon addClassNames='text-gray-800' /></span>
                                                     <span>Lịch sử đặt hàng</span>
@@ -173,7 +175,6 @@ const redirectHistoryOrder= ()=>{
                                             </div>
                                         </div>
                                     </div>
-
                                 </div>
                             ) : (
                                 <>
@@ -187,7 +188,7 @@ const redirectHistoryOrder= ()=>{
                                     </div>
                                 </>
                             )}
-                            <div className='flex gap-4'>
+                            <div className='flex gap-4 max-md:gap-2'>
                                 <AppIcons.ChatIcon addClassNames='text-[#fff] cursor-pointer' width='20px' height='20px' />
                                 <div className='w-[2px] h-[20px] bg-[#fff] rounded-md'></div>
                                 <Link to={'/gio-hang'}>
@@ -208,10 +209,27 @@ const redirectHistoryOrder= ()=>{
                 </div>
             </header>
             <nav>
-                <div className=" border-1 w-[100%]  p-4 shadow-lg bg-[#fff]">
-                    <div className=" flex justify-between items-center gap-2 w-[90%] m-auto">
+                <div className=" border-1 w-[100%] p-4 shadow-lg bg-[#fff] max-md:mt-[45px]  ">
+                    <div
+                        className="cursor-pointer hidden max-md:block"
+                        onClick={() => setIsShowCategory(!isShowCategory)}
+                    >
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-center gap-3">
+                                <AppIcons.CategoryIcon addClassNames="text-[#2563eb]" />
+                                <span className="text-[#2563eb] font-semibold">
+                                    Xem Tất Cả Danh Mục Thuốc
+                                </span>
+                            </div>
+                            <AppIcons.ArrowDown
+                                addClassNames={`bg-[#2563eb] rounded-[50%] transition-transform duration-300 ${isShowCategory ? 'rotate-180' : ''
+                                    }`}
+                            />
+                        </div>
+                    </div>
+                    <div className={` ${!isShowCategory ? 'max-md:overflow-hidden max-md:h-0' : 'max-md-max-h-100'} flex justify-between items-center gap-2 w-[90%] max-md:w-full m-auto max-md:flex-col`}>
                         {initialCategoryData && initialCategoryData.map((category) => (
-                            <div className="flex items-center group relative" key={category._id}>
+                            <div className="flex items-center group relative max-md:flex max-md:justify-between max-md:w-full max-md:pt-4 " key={category._id}>
                                 <Link to={`/danh-muc/${category._id}`}>
                                     <span className="w-max cursor-pointer hover:text-[#2563EB] duration-200 text-[15px] font-semibold">{category?.name}</span>
                                 </Link>
@@ -220,12 +238,12 @@ const redirectHistoryOrder= ()=>{
                                         <AppIcons.ArrowDown
                                             addClassNames="inline-block ml-1 transition-transform duration-300 group-hover:rotate-180 group-hover:text-[#2563EB]"
                                         />
-                                        <div className='absolute left-[-5px] pt-[17px] z-20'>
-                                            <div className="hidden max-h-0 overflow-hidden group-hover:max-h-[500px] duration-500 group-hover:duration-500 group-hover:block bg-[#fff] w-[250px] shadow-2xl p-4 rounded-[5px] ">
+                                        <div className='absolute left-[-5px] pt-[17px] z-20 max-md:w-[104%] max-md:top-[10] max-md:left-[-20px]'>
+                                            <div className="hidden max-h-0 overflow-hidden group-hover:max-h-[500px] duration-500 group-hover:duration-500 group-hover:block bg-[#fff] w-[253px] shadow-2xl p-4 rounded-[5px] max-md:w-[100%]  max-md:pb-3 max-md:ml-1">
                                                 {category.subcategories.map((sub) => (
                                                     <div key={sub._id}>
                                                         <Link to={`/danh-muc/san-pham-danh-muc/${sub._id}`}>
-                                                            <span className="block py-2 text-sm text-gray-600 hover:text-[#2563EB] w-max font-bold">
+                                                            <span className="block py-2 text-sm text-gray-600 hover:text-[#2563EB] w-max font-bold max-md:pb-5 cursor-pointer">
                                                                 {sub?.name}
                                                             </span>
                                                         </Link>
@@ -237,13 +255,13 @@ const redirectHistoryOrder= ()=>{
                                 )}
                             </div>
                         ))}
-                        <div className="flex items-center group relative" >
-                            <span className="w-max cursor-pointer hover:text-[#2563EB] duration-200 text-[15px] font-semibold">Góc sống khỏe</span>
+                        <div className="flex items-center group relative max-md:flex max-md:justify-between max-md:w-full" >
+                            <span className="w-max cursor-pointer hover:text-[#2563EB] duration-200 text-[15px] font-semibold max-md:pt-4">Góc sống khỏe</span>
                             <div className="ml-3">
                                 <AppIcons.ArrowDown
                                     addClassNames="inline-block ml-1 transition-transform duration-300 group-hover:rotate-180 group-hover:text-[#2563EB]"
                                 />
-                                <div className='absolute left-[-5px] pt-[17px] z-20'>
+                                <div className='absolute left-[-30px] pt-[17px] z-20'>
                                     <div className="hidden max-h-0 overflow-hidden group-hover:max-h-[500px] duration-500 group-hover:duration-500 group-hover:block bg-[#fff] w-[250px] shadow-2xl p-4 rounded-[5px]">
                                         <Link to={PATH_ROUTERS_CLIENT.BLOG}>
                                             <span className="block py-2 text-sm text-gray-600 hover:text-[#2563EB] w-max font-bold cursor-pointer">
@@ -281,14 +299,10 @@ const redirectHistoryOrder= ()=>{
                                 </div>
                             </div>
                         </div>
-                        <div className="flex items-center group relative" >
-                            <Link>
-                                <span className="w-max cursor-pointer hover:text-[#2563EB] duration-200 text-[15px] font-semibold">Nhà thuốc</span>
-                            </Link>
-                        </div>
                     </div>
                 </div>
             </nav>
+            
         </div>
     );
 };
