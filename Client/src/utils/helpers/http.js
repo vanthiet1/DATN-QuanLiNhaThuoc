498e86a31ef11640599312cd3518ef4719b85e37
van Thiết <tretrauthiet@gmail.com>
2024-11-28 01:01:02 +0700

    xóa space line

--- Client/src/utils/helpers/http.js
+++ Client/src/utils/helpers/http.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
-
 const http = axios.create({ 
-    baseURL: 'http://localhost:8080/api/v1/api/v1',
+    baseURL: 'http://localhost:8080/api/v1/',
     // timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
