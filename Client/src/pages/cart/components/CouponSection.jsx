7de182955c497d63a1b97d2f910eb5abd6804e49
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-12-17 19:44:58 +0700

    cập nhật điều điện để thêm phiếu giảm giá

--- Client/src/pages/cart/components/CouponSection.jsx
+++ Client/src/pages/cart/components/CouponSection.jsx
@@ -4,37 +4,44 @@ import couponServices from '../../../services/couponService';
 import { useCartFormContext } from './context/CartFormProvider';
 import formatsHelper from '../../../utils/helpers/formats';
 import { uesCheckOutContext } from './context/CheckOutProvider';
+import { useCartContext } from '../../../contexts/CartContext';
+import { PRICE_FOR_COUPON } from '../../../utils/constant/common';
 
 const CouponSection = () => {
   const { register } = useCartFormContext();
   const { responsData: couponsData } = useFetch(couponServices.getCouponsActive, {}, []);
   const { handleApplyCoupon } = uesCheckOutContext();
+  const { totalPrice } = useCartContext();
 
   return (
-    <div className='max-h-[200px] overflow-y-auto'>
-      <h2 className='text-lg font-medium text-gray-700 mb-4'>Sử dụng mã giảm giá</h2>
-      <div>
-        {couponsData &&
-          couponsData.length > 0 &&
-          couponsData.map((coupon) => {
-            const { _id, code, discount_value } = coupon;
-            return (
-              <label htmlFor={_id} key={_id}>
-                <div className='flex items-center gap-3 mb-3 '>
-                  <InputRadio
-                    refinput={register('coupon_id')}
-                    id={_id}
-                    defaultValue={_id}
-                    onChange={(e) => handleApplyCoupon(e, discount_value)}
-                  />
-                  <p>nhập mã {code}</p>
-                  <span>Giảm {formatsHelper.currency(discount_value)}</span>
-                </div>
-              </label>
-            );
-          })}
-      </div>
-    </div>
+    <>
+      {totalPrice > PRICE_FOR_COUPON && (
+        <div className='max-h-[200px] overflow-y-auto'>
+          <h2 className='text-lg font-medium text-gray-700 mb-4'>Sử dụng mã giảm giá</h2>
+          <div>
+            {couponsData &&
+              couponsData.length > 0 &&
+              couponsData.map((coupon) => {
+                const { _id, code, discount_value } = coupon;
+                return (
+                  <label htmlFor={_id} key={_id}>
+                    <div className='flex items-center gap-3 mb-3 '>
+                      <InputRadio
+                        refinput={register('coupon_id')}
+                        id={_id}
+                        defaultValue={_id}
+                        onChange={(e) => handleApplyCoupon(e, discount_value)}
+                      />
+                      <p>nhập mã {code}</p>
+                      <span>Giảm {formatsHelper.currency(discount_value)}</span>
+                    </div>
+                  </label>
+                );
+              })}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
