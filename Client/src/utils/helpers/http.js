b9af59008d64be62eea5e9efa6e31a5e5da7ce94
DucLong-PD07979 <127298706+DucLong-PD07979@users.noreply.github.com>
2024-12-17 21:20:10 +0700

    Update http.js

--- Client/src/utils/helpers/http.js
+++ Client/src/utils/helpers/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const http = axios.create({ 
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: 'https://datn-quanlinhathuoc.onrender.com/api/v1',
     headers: {
         'Content-Type': 'application/json'
         // 'Content-Type': 'multipart/form-data' 
