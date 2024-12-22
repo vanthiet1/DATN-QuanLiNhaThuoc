2a18fcf3e29a16b22293367386ca9acb5bdc4132
LongNguyen25012002 <25nguyenduclong@gmail.com>
2024-12-23 04:28:44 +0700

    thêm gợi ý để trải nghiệm toàn bộ chức năng

--- Client/src/contexts/GlobalContext.jsx
+++ Client/src/contexts/GlobalContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import UserProvider from './UserContext';
 import ToggleFormProvider from './ToggleFormContext';
@@ -7,10 +7,36 @@ import TabUIAccountProvider from './TabUIAccountContext';
 import CartProvider from './CartContext';
 import { ConfirmDialogProvider } from '../components/dialog/ConfirmDialogContext';
 import SocketProvider from './SocketContext';
+import DiaLog from '../components/dialog/DiaLog';
 const GlobalContext = createContext();
 
+const DialogRecommend = ({ visited, onVisited }) => {
+  if (visited) {
+    return (
+      <DiaLog isOpen={visited} onClose={() => onVisited(!visited)}>
+        <h3 className='text-gray-700 font-medium'>👋 Chào mừng bạn đến với nhà thuốc Bình An Dược </h3>
+        <div className='mt-2'>
+          <p className='text-base text-blue-500 '>Gợi ý cho bạn!</p>
+          <p className='max-w-[73%] text-gray-700'>
+            Để xem đầy đủ chức năng của admin bạn có thể đăng nhập với tài khoản: admin@gmail.com - mk :123123123
+          </p>
+          <p className='max-w-[73%] text-gray-700'>Sau khi đăng nhập di chuột vào avatar để tới trang quản lý 🩷🩷</p>
+        </div>
+      </DiaLog>
+    );
+  }
+  return <></>;
+};
 const GlobalContextProvider = ({ children }) => {
   const [visited, setVisited] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisited(true);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <GlobalContext.Provider value={visited}>
       <SocketProvider>
@@ -28,6 +54,7 @@ const GlobalContextProvider = ({ children }) => {
           </GoogleOAuthProvider>
         </ConfirmDialogProvider>
       </SocketProvider>
+      <DialogRecommend visited={visited} onVisited={setVisited} />
     </GlobalContext.Provider>
   );
 };
