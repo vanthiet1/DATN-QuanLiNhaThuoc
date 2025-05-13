b2656327bd65532ef5cfdece7e37514c86864372
Văn Thiết ( Frontend DEV ) <125172255+vanthiet1@users.noreply.github.com>
2025-05-13 11:32:43 +0700

    Update http.js

--- Client/src/utils/helpers/http.js
+++ Client/src/utils/helpers/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const http = axios.create({ 
-    baseURL: 'https://binh-an-duoc-production.up.railway.app/api/v1',
+    baseURL: 'https://datn-quanlinhathuoc-production.up.railway.app/api/v1',
     headers: {
         'Content-Type': 'application/json'
         // 'Content-Type': 'multipart/form-data' 
